5dd09af2653781f3239d1adf6225723e29c8dfe3
Hisam Fahri <me@hisamafahri.com>
2022-03-08 14:38:16 +0700

    feat(cmd): add add type prompt

--- cmd/add/add.go
+++ cmd/add/add.go
@@ -1,6 +1,11 @@
 package add
 
 import (
+	"fmt"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/hisamafahri/rite/helper"
+	"github.com/hisamafahri/rite/model"
 	"github.com/spf13/cobra"
 )
 
@@ -9,6 +14,20 @@ var AddCmd = &cobra.Command{
 	Use:   "add",
 	Short: "generate user, create new group, add new files",
 	Run: func(cmd *cobra.Command, args []string) {
-		addUser()
+		addType := struct {
+			Type string `survey:"type"`
+		}{}
+
+		// perform the questions
+		err := survey.Ask(model.AddPrompt(), &addType)
+		helper.CheckErr(err)
+
+		if addType.Type == "New Groups" {
+			fmt.Print("New Groups")
+		} else if addType.Type == "New Files" {
+			fmt.Println("new Files")
+		} else {
+			addUser()
+		}
 	},
 }
---
+++ model/add.survey.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"github.com/AlecAivazis/survey/v2"
+)
+
+// the questions to ask
+func AddPrompt() []*survey.Question {
+	return []*survey.Question{
+		{
+			Name: "type",
+			Prompt: &survey.Select{
+				Message: "What you want to add?",
+				Options: []string{"New Groups", "New Files", "New User"},
+			},
+			Validate: survey.Required,
+		},
+	}
+}
